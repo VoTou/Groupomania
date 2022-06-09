940ced6c63433cefeef183b82e24d4da4edbcecc
valentin_noirbenne <valentin.noirbenne@laposte.net>
2022-06-09 14:36:21 +0200

    Ajout du middleware pour obliger l'authentification

--- back/server.js
+++ back/server.js
@@ -3,15 +3,17 @@ const cookieParser = require("cookie-parser");
 const userRoutes = require("./routes/user.routes");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
-const { checkUser } = require("./middleware/auth.middleware");
+const { checkUser, requireAuth } = require("./middleware/auth.middleware");
 const app = express();
-
 
 app.use(express.json());
 app.use(cookieParser());
 
 // jwt
-app.get('*', checkUser);
+app.get("*", checkUser);
+app.get("/jwtid", requireAuth, (req, res) => {
+  res.status(200).send(res.locals.user._id);
+});
 
 // routes
 app.use("/api/user", userRoutes);
