ef974c75eeea347fb5a083596fe077dc72537886
valentin_noirbenne <valentin.noirbenne@laposte.net>
2022-06-09 13:26:28 +0200

    Création route signUp et importation du parser avec express

---
+++ back/models/user.model.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      max: 1024,
+      minlength: 6
+    },
+    picture: {
+      type: String,
+      default: "./uploads/profil/random-user.png"
+    },
+    bio :{
+      type: String,
+      max: 1024,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+// Fonctions de cryptage du password avant sauvegarde dans la DB
+userSchema.pre("save", async function(next) {
+  const salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+// Fonction pour comparer les infos utilisateurs avec celles de la base de données
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('Incorrect password');
+  }
+  throw Error('Incorrect email')
+};
+
+userSchema.plugin(uniqueValidator);
+
+const UserModel = mongoose.model("user", userSchema);
+
+
+module.exports = UserModel;
