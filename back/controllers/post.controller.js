cd0a46f2f3af2dcc1200ad3c7935525f3396a3bc
valentin_noirbenne <valentin.noirbenne@laposte.net>
2022-06-09 17:13:25 +0200

    Création route updatePost

--- back/controllers/post.controller.js
+++ back/controllers/post.controller.js
@@ -30,8 +30,26 @@ module.exports.readPost = (req, res) => {
 
 // Mise à jour d'un post
 module.exports.updatePost = (req, res) => {
-    
+    if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("ID unknown : " + req.params.id);
+
+  const updatedRecord = {
+    message: req.body.message,
+  };
+
+  PostModel.findByIdAndUpdate(
+    req.params.id,
+    { $set: updatedRecord },
+    { new: true },
+    (err, docs) => {
+      if (!err) res.send(docs);
+      else console.log("Update error : " + err);
+    }
+  );
 };
 
+
 // Suppression d'un post
-module.exports.deletePost = (req, res) => {};
+module.exports.deletePost = (req, res) => {
+    
+};
