cdf8ef4771cbb8caa22443c84c06b2c54016fba2
valentin_noirbenne <valentin.noirbenne@laposte.net>
2022-06-09 17:09:07 +0200

    Création route readPost

--- back/controllers/post.controller.js
+++ back/controllers/post.controller.js
@@ -1,11 +1,10 @@
-const postModel = require("../models/post.model");
 const PostModel = require("../models/post.model");
 const UserModel = require("../models/user.model");
 const ObjectID = require("mongoose").Types.ObjectId;
 
 // Création d'un post
 module.exports.createPost = async (req, res) => {
-  const newPost = new postModel({
+  const newPost = new PostModel({
     posterId: req.body.posterId,
     message: req.body.message,
     video: req.body.video,
@@ -22,15 +21,17 @@ module.exports.createPost = async (req, res) => {
 };
 
 // Lecture d'un post
-module.exports.readPost = (res, req) => {
+module.exports.readPost = (req, res) => {
   PostModel.find((err, docs) => {
     if (!err) res.send(docs);
     else console.log("Error to get data : " + err);
-  }).sort({ createdAt: -1 });
+  });
 };
 
 // Mise à jour d'un post
-module.exports.updatePost = (res, req) => {};
+module.exports.updatePost = (req, res) => {
+    
+};
 
 // Suppression d'un post
-module.exports.deletePost = (res, req) => {};
+module.exports.deletePost = (req, res) => {};
