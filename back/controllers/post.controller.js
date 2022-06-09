41efb8563c0d3732cc64395823d3e50612df6dad
valentin_noirbenne <valentin.noirbenne@laposte.net>
2022-06-09 16:23:52 +0200

    Création du squelette des routes CRUD post

---
+++ back/controllers/post.controller.js
@@ -0,0 +1,19 @@
+const PostModel = require("../models/post.model");
+const UserModel = require("../models/user.model");
+const ObjectID = require("mongoose").Types.ObjectId;
+
+module.exports.readPost = (res, req) => {
+
+}
+
+module.exports.createPost = (res, req) => {
+
+}
+
+module.exports.updatePost = (res, req) => {
+
+}
+
+module.exports.deletePost = (res, req) => {
+
+}
