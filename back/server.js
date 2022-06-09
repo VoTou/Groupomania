cd18d9fdebd23faca8b3deb8c70112bef1e6649f
valentin_noirbenne <valentin.noirbenne@laposte.net>
2022-06-09 13:07:04 +0200

    Création du server.js

--- back/server.js
+++ back/server.js
@@ -3,6 +3,7 @@ require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
 const app = express();
 
+
 // server
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
