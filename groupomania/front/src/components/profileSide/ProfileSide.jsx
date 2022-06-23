259631b7dda288ca58f855ceeb9b0d677e1d4ed5
valentin_noirbenne <valentin.noirbenne@laposte.net>
2022-06-23 10:06:36 +0200

    Ajout des images profil et de la recherche

---
+++ groupomania/front/src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LogoSearch from "../logoSearch/LogoSearch";
+import ProfileCard from "../ProfileCard/ProfileCard";
+
+const ProfileSide = () => {
+  return <div className="ProfileSide">
+    <LogoSearch />
+    <ProfileCard />
+  </div>;
+};
+
+export default ProfileSide;
