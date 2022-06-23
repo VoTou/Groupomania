b0b745b6be8ab98ebe20f7b083062632c192070c
valentin_noirbenne <valentin.noirbenne@laposte.net>
2022-06-23 10:28:12 +0200

    Création de ProfileCard

--- groupomania/front/src/components/ProfileCard/ProfileCard.jsx
+++ groupomania/front/src/components/ProfileCard/ProfileCard.jsx
@@ -11,9 +11,31 @@ const ProfileCard = () => {
             <img src={Cover} alt="" />
             <img src={Profile} alt="" />
         </div>
-        
+        <div className="ProfileName">
+            <span>Zendaya MJ</span>
+            <span>Senior UI/UX Designer</span>
+        </div>
+        <div className="followStatus">
+            <hr />
+            <div>
+                <div className="follow">
+                    <span>6,890</span>
+                    <span>Followings</span>
+                </div>
+                <div className="verticalLign"></div>
+                <div className="follow">
+                    <span>1</span>
+                    <span>Followers</span>
+                </div>
+            </div>
+            <hr />
+        </div>
+
+        <span>
+            My Profile
+        </span>
       </div>
-    
+
   )
 }
 
