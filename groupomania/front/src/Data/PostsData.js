934d20a87cdeb05633b613e3afcb90fa92ae5d08
valentin_noirbenne <valentin.noirbenne@laposte.net>
2022-06-23 16:08:18 +0200

    Création de la section post

---
+++ groupomania/front/src/Data/PostsData.js
@@ -0,0 +1,27 @@
+import postPic1 from '../img/postpic1.jpg'
+import postPic2 from '../img/postpic2.jpg'
+import postPic3 from '../img/postpic3.JPG'
+
+export const PostsData = [
+    {
+        img: postPic1,
+        name: 'Tzuyu',
+        desc: 'Happy New Year all friends ! #2022',
+        likes: 2300,
+        liked: true
+    },
+    {
+        img: postPic2,
+        name: 'Maryam',
+        desc: 'Happy New Year all friends ! #2023',
+        likes: 199,
+        liked: false
+    },
+    {
+        img: postPic3,
+        name: 'Fred',
+        desc: 'Happy New Year all friends ! #2024',
+        likes: 1200,
+        liked: true
+    }
+];
