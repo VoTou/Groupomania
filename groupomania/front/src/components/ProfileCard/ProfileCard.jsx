0fd996bccb70abb2de2923adf6b0484f42459bd5
valentin_noirbenne <valentin.noirbenne@laposte.net>
2022-06-23 10:05:58 +0200

    Ajout de la barre de recherche et de la partie profil

---
+++ groupomania/front/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Cover from '../../img/cover.jpg'
+import Profile from '../../img/profileImg.jpg'
+import './ProfileCard.css'
+
+const ProfileCard = () => {
+  return (
+    
+      <div className="ProfileCard">
+        <div className="ProfileImages">
+            <img src={Cover} alt="" />
+            <img src={Profile} alt="" />
+        </div>
+        
+      </div>
+    
+  )
+}
+
+export default ProfileCard
