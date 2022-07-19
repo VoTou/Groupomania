dc36ee81bbecefaef948ba7b3449184bb885a810
Noirbenne Valentin <valentin.noirbenne@laposte.net>
2022-07-19 21:25:07 +0200

    Modif uppercase

--- groupomania/server/Models/userModel.js
+++ groupomania/server/Models/userModel.js
@@ -4,9 +4,11 @@ const UserSchema = mongoose.Schema(
   {
     username: {
       type: String,
+      required: true,
       trim: true,
+      uppercase: true,
       minlength: 3,
-      maxlenth: 40,
+      maxlength: 40,
     },
     password: {
       type: String,
